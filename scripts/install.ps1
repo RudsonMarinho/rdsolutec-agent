a2f8ed5ce9e4db4c69d0a0f66ea2d051632df42a
RudsonMarinho <127106898+RudsonMarinho@users.noreply.github.com>
2026-04-22 23:25:01 -0300

    Add WinRAR to installation script

--- scripts/install.ps1
+++ scripts/install.ps1
@@ -16,7 +16,8 @@ $programas = @(
     @{ nome="Panda AV"; url="https://github.com/RudsonMarinho/rdsolutec-agent/releases/download/v1.0.0/PANDAFREEAV.exe"; arquivo="panda.exe"; tipo="exe" },
     @{ nome="Adobe Reader"; url="https://github.com/RudsonMarinho/rdsolutec-agent/releases/download/v1.0.0/Reader_br_install.exe"; arquivo="reader.exe"; tipo="exe" },
     @{ nome="Zoom"; url="https://github.com/RudsonMarinho/rdsolutec-agent/releases/download/v1.0.0/ZoomInstallerFull.exe"; arquivo="zoom.exe"; tipo="exe" },
-    @{ nome="Whatsapp"; url="https://github.com/RudsonMarinho/rdsolutec-agent/releases/download/v1.0.0/WhatsApp.exe"; arquivo="zoom.exe"; tipo="exe" }
+    @{ nome="Whatsapp"; url="https://github.com/RudsonMarinho/rdsolutec-agent/releases/download/v1.0.0/WhatsApp.exe"; arquivo="zoom.exe"; tipo="exe" },
+    @{ nome="WinRAR"; url="https://github.com/RudsonMarinho/rdsolutec-agent/releases/download/v1.0.0/winrar-x64-701br.exe"; arquivo="zoom.exe"; tipo="exe" }
 )
 
 # ===== FORM =====
