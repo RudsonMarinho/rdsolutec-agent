f71e9a8596e4cd1aab5dacf731c59971694d198d
RudsonMarinho <127106898+RudsonMarinho@users.noreply.github.com>
2026-04-22 23:33:18 -0300

    Remove duplicate entry for Firefox in install script

--- scripts/install.ps1
+++ scripts/install.ps1
@@ -19,8 +19,7 @@ $programas = @(
     @{ nome="Whatsapp"; url="https://github.com/RudsonMarinho/rdsolutec-agent/releases/download/v1.0.0/WhatsApp.exe"; arquivo="zoom.exe"; tipo="exe" },
     @{ nome="WinRAR"; url="https://github.com/RudsonMarinho/rdsolutec-agent/releases/download/v1.0.0/winrar-x64-701br.exe"; arquivo="zoom.exe"; tipo="exe" },
     @{ nome="CPU-Z"; url="https://github.com/RudsonMarinho/rdsolutec-agent/releases/download/v1.0.0/cpu-z_2.19-en.exe"; arquivo="zoom.exe"; tipo="exe" },
-    @{ nome="FoxitPDF"; url="https://github.com/RudsonMarinho/rdsolutec-agent/releases/download/v1.0.0/FoxitPDFReader20261_L10N_Setup_Prom_x64.exe; tipo="exe" },
-    @{ nome="Firefox"; url="https://github.com/RudsonMarinho/rdsolutec-agent/releases/download/v1.0.0/Firefox.Installer.exe; tipo="exe" }
+    @{ nome="FoxitPDF"; url="https://github.com/RudsonMarinho/rdsolutec-agent/releases/download/v1.0.0/FoxitPDFReader20261_L10N_Setup_Prom_x64.exe; tipo="exe" }
     
 )
 
