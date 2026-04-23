cb41ca2bdb83017e044f96e0f1ff607b296f00ab
RudsonMarinho <127106898+RudsonMarinho@users.noreply.github.com>
2026-04-22 23:37:33 -0300

    Update install.ps1

--- scripts/install.ps1
+++ scripts/install.ps1
@@ -16,11 +16,10 @@ $programas = @(
     @{ nome="Panda AV"; url="https://github.com/RudsonMarinho/rdsolutec-agent/releases/download/v1.0.0/PANDAFREEAV.exe"; arquivo="panda.exe"; tipo="exe" },
     @{ nome="Adobe Reader"; url="https://github.com/RudsonMarinho/rdsolutec-agent/releases/download/v1.0.0/Reader_br_install.exe"; arquivo="reader.exe"; tipo="exe" },
     @{ nome="Zoom"; url="https://github.com/RudsonMarinho/rdsolutec-agent/releases/download/v1.0.0/ZoomInstallerFull.exe"; arquivo="zoom.exe"; tipo="exe" },
-    @{ nome="Whatsapp"; url="https://github.com/RudsonMarinho/rdsolutec-agent/releases/download/v1.0.0/WhatsApp.exe"; arquivo="zoom.exe"; tipo="exe" },
-    @{ nome="WinRAR"; url="https://github.com/RudsonMarinho/rdsolutec-agent/releases/download/v1.0.0/winrar-x64-701br.exe"; arquivo="zoom.exe"; tipo="exe" },
-    @{ nome="CPU-Z"; url="https://github.com/RudsonMarinho/rdsolutec-agent/releases/download/v1.0.0/cpu-z_2.19-en.exe"; arquivo="zoom.exe"; tipo="exe" },
-    @{ nome="FoxitPDF"; url="https://github.com/RudsonMarinho/rdsolutec-agent/releases/download/v1.0.0/FoxitPDFReader20261_L10N_Setup_Prom_x64.exe; tipo="exe" }
-    
+    @{ nome="WhatsApp"; url="https://github.com/RudsonMarinho/rdsolutec-agent/releases/download/v1.0.0/WhatsApp.exe"; arquivo="whatsapp.exe"; tipo="exe" },
+    @{ nome="WinRAR"; url="https://github.com/RudsonMarinho/rdsolutec-agent/releases/download/v1.0.0/winrar-x64-701br.exe"; arquivo="winrar.exe"; tipo="exe" },
+    @{ nome="CPU-Z"; url="https://github.com/RudsonMarinho/rdsolutec-agent/releases/download/v1.0.0/cpu-z_2.19-en.exe"; arquivo="cpuz.exe"; tipo="exe" },
+    @{ nome="FoxitPDF"; url="https://github.com/RudsonMarinho/rdsolutec-agent/releases/download/v1.0.0/FoxitPDFReader20261_L10N_Setup_Prom_x64.exe"; arquivo="foxit.exe"; tipo="exe" }
 )
 
 # ===== FORM =====
